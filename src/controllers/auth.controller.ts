3b2dd6c12d593b90914c66f39f21fe9ad8649f60
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-21 15:34:37 -0500

    feat: adding email validation

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,15 +1,32 @@
 import { Request, Response } from 'express';
+import User from '../models/User';
 
-export const createNewUser = (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
+export const createNewUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
 
-  return res.status(201).json({
-    ok: true,
-    message: 'New User',
-    name,
-    email,
-    password,
-  });
+    if (user)
+      return res.status(400).json({
+        ok: false,
+        message: 'El correo ya ha sido registrado por otro usuario',
+      });
+
+    user = new User(req.body);
+    await user.save();
+
+    return res.status(201).json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      message: 'Ocurrió un error',
+    });
+  }
 };
 
 export const login = (req: Request, res: Response) => {
