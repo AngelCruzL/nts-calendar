da609eaaa3fdd52b782aad47f10ddcab0ea01dbc
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-21 12:23:30 -0500

    chore: setting up express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hola mundo')
+import express from 'express'
+import * as config from './config'
+
+const app = express()
+
+app.listen(config.SERVER_PORT, () => {
+  console.log('hi')
+})
