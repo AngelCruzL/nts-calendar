40419a6c2a3858f120ac880b9e33d30a97d5af15
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-21 20:18:18 -0500

    feat: adding cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 import config from './config';
 import authRoutes from './routes/auth.routes';
 import './database';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
